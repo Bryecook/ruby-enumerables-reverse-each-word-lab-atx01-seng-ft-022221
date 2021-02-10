b3a8905306288a942e0da8824f242772909a62dc
Brian Cook <brybcook@gmail.com>
2021-02-10 16:18:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(sentence)
   sentence.split.each{|element|reversed << "#{element.reverse} "}
   reversed.strip
 
+  array=[]
   sentence.split.collect do |array|
     array.reverse
   end.join(" ")
-array
 end
