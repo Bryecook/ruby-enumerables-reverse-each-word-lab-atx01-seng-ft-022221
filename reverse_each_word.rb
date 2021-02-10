8582f07f491d1751ff7a4a4a20969570d7e755e2
Brian Cook <brybcook@gmail.com>
2021-02-10 16:05:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 require "pry"
 def reverse_each_word(sentence)
   reversed=""
-  array=sentence
-  array.split.each{|element|reversed << "#{element.reverse}"}
+  sentence.split.each{|element|reversed << "#{element.reverse}"}
   array
 end
